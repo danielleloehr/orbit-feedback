933b84a7419f6f311c9b1558eccbd28a4e6d0111
danielleloehr <danielle.loehr@helmholtz-berlin.de>
2024-01-10 15:11:00 +0100

    sockets files

--- test-unit/sockets.h
+++ test-unit/sockets.h
@@ -1,9 +1,6 @@
 #ifndef SOCKETS_H
 #define SOCKETS_H
 
-#include <stdio.h>
-#include <errno.h>      // perror()
-
 /* Network */
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -17,39 +14,10 @@ static char IP_ADDR[16] = "1.1.2.1";
 /* Libera default port      */
 static int PORT = 2048;
 
-/* Main server information                          
-   Forward compressed packets to this address       */
-static char LOCAL_ADDR[16] = "192.168.1.200"; 
-static int LOCAL_PORT = 2049;
 
-
-int prepare_socket(struct sockaddr_in server, int listen) {
-    int socket_desc;
-
-    /* Create a datagram socket*/
-    if ((socket_desc = socket(AF_INET, SOCK_DGRAM, 0)) < 0){
-        perror("socket()");
-        return -1;
-    }
-
-    /* Set up Listening for the RECEIVING socket */
-    if(listen){
-        /* Bind server to this socket.*/
-        server.sin_family      = AF_INET;       /* Server is in Internet Domain */
-        server.sin_port        = htons(PORT);         
-        server.sin_addr.s_addr = inet_addr(IP_ADDR); 
-
-        if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0){
-            perror("bind()");
-            return -1;
-        }
-        
-    }else{
-        server.sin_family      = AF_INET;            
-        server.sin_addr.s_addr = inet_addr(LOCAL_ADDR);
-    }
-
-    return socket_desc;
-}
+/************************************
+ * FUNCTION PROTOTYPES
+ ************************************/
+int prepare_socket(struct sockaddr_in server);
 
 #endif
